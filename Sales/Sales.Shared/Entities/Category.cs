290e28037d65542247f74233e207372e9446a94d
afelipeberrio <andres.b@2nv.co>
2023-02-23 09:27:37 -0500

    Creation Table Categories

---
+++ Sales/Sales.Shared/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Categoria")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public String Name { get; set; } = null!;
+    }
+}
