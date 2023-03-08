3c893fdf0c76b3960a89233ab2793772fb59a0ed
afelipeberrio <andres.b@2nv.co>
2023-03-08 12:55:22 -0500

    Security hardcoded on Web

---
+++ Sales/Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var andresUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Andres"),
+                new Claim("LastName", "Berrio"),
+                new Claim(ClaimTypes.Name, "andres@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+
+            },
+            authenticationType: "test");
+            
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+        }
+
+    }
+}
